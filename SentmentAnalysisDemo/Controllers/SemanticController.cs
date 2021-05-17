26f74ec7ffebaf0e564ee8a73e2c9a4ce939bfb7
Manali Jain <Manali.Jain@kanini.com>
2021-05-17 18:12:06 +0530

    Add project files.

---
+++ SentmentAnalysisDemo/Controllers/SemanticController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using Sentiment_AnalysisML.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SentmentAnalysisDemo.Controllers
+{
+    public class SemanticController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Analysis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Analysis(ModelInput input)
+        {
+            // Load the model  
+            MLContext mlContext = new MLContext();
+            ITransformer mlModel = mlContext.Model.Load(@"..\Sentiment AnalysisML.Model\MLModel.zip", out var modelInputSchema);
+            // Create predection engine related to the loaded train model
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+            //Input  
+            input.Year = DateTime.Now.Year;
+            // Try model on sample data and find the score
+            ModelOutput result = predEngine.Predict(input);
+            // Store result into ViewBag
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
---
+++ SentmentAnalysisDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SentmentAnalysisDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
